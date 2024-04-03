0d89ad2360f0200609c9f1c2f9aa78ea3efadb9d
wallacewong <wallacepuit@gmail.com>
2024-04-03 14:55:03 +0800

    feat: add locale provider

--- types/index.ts
+++ types/index.ts
@@ -8,3 +8,14 @@ type Banner = {
   createdAt: Date
   updatedAt: Date
 }
+
+type Category = {
+  _id: string
+  name: string,
+  priority: number,
+  shop: string,
+  status: "ACTIVE" | "INACTIVE"
+  createdAt: Date
+  updatedAt: Date
+  children: Category[]
+}
