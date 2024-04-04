309455d9d512d2af69a0ca6b78f311568c89ec07
wallacewong <wallacepuit@gmail.com>
2024-04-04 12:13:59 +0800

    feat: bottom sheet on home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
 import { FlatList, RefreshControl, SectionList } from 'react-native';
+import { Button, Sheet } from 'tamagui';
 import { YStack, Image, Text, SizableText, Spinner } from 'tamagui';
 import { listAdsBanners } from '~/api/adsBanner';
 import { listCategories, listProducts } from '~/api/product';
@@ -13,8 +14,13 @@ const Home = () => {
   const { t } = useLocale()
   const [selectedCategoryId, setSelectedCategoryId] = useState()
 
+  const [categoriesOptions, setCategoriesOptions] = useState()
   const { data: adsBanners = [], isFetching: isAdsBannersFetching } = useQuery({ queryKey: ['adsBanners'], queryFn: listAdsBanners })
   const { data: categories = [], isFetching: isCategoriesFetching } = useQuery({ queryKey: ['categories'], queryFn: listCategories })
+
+  const [isCategorySheetOpen, setIsCategorySheetOpen] = useState(false)
+  const [sheetPosition, setSheetPosition] = useState(0)
+
 
   const { data: products,
     isFetching: isProductFetching,
@@ -66,6 +72,8 @@ const Home = () => {
   }
 
   const renderItem = ({ item, index, section }) => {
+    const categoryName = selectedCategoryId ? categoriesOptions?.find((f) => { return f.value == selectedCategoryId }).name : t('all')
+    const categoryLabel = selectedCategoryId ? categoriesOptions?.find((f) => { return f.value == selectedCategoryId }).label : t('hotItems')
 
     switch (section.key) {
       case 'photos':
@@ -75,8 +83,11 @@ const Home = () => {
           />
         )
       case 'menu':
+
         return (
-          <></>
+          <Button onPress={() => setIsCategorySheetOpen(true)}>
+            <Text>{categoryName}</Text>
+          </Button>
         )
       case 'recommendedProducts':
         return (
@@ -95,7 +106,7 @@ const Home = () => {
                 return null
               }
               return (
-                <Spinner color="$color.primary"/>
+                <Spinner color="$color.primary" />
               )
             }}
           />
@@ -130,7 +141,31 @@ const Home = () => {
         style={{ backgroundColor: '#fff' }}
         keyExtractor={(item, index) => item + index}
       />
+      <Sheet
+        forceRemoveScrollEnabled={isCategorySheetOpen}
+        modal={true}
+        open={isCategorySheetOpen}
+        onOpenChange={setIsCategorySheetOpen}
+        snapPointsMode={"percent"}
+        snapPoints={[60]}
+        dismissOnSnapToBottom
+        position={sheetPosition}
+        onPositionChange={setSheetPosition}
+        zIndex={100_000}
+        animation="quick"
+      >
+        <Sheet.Overlay
+          style={{ opacity: 0.8, backgroundColor: "lightslategrey" }}
+          animation="lazy"
+          enterStyle={{ opacity: 0 }}
+          exitStyle={{ opacity: 0 }}
+        />
+        <Sheet.Handle backgroundColor={"ghostwhite"} />
 
+        <Sheet.Frame padding="$4" justifyContent="center" alignItems="center" space="$5" backgroundColor={"ghostwhite"} >
+          <Text>hello world</Text>
+        </Sheet.Frame>
+      </Sheet>
     </YStack>
   );
 }
