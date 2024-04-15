594cd98bb887a6940f237d7a59f9e0fc0078f7de
wallacewong <wallacepuit@gmail.com>
2024-04-15 17:23:21 +0800

    style: formatting in coupon layout

--- app/coupon/[couponId].tsx
+++ app/coupon/[couponId].tsx
@@ -2,7 +2,7 @@ import { useLocalSearchParams } from "expo-router"
 import { SizableText } from "tamagui"
 
 const CouponDetail = () => {
-  const {couponId}=useLocalSearchParams()
+  const { couponId } = useLocalSearchParams()
   return (
     <SizableText>
       {couponId}
