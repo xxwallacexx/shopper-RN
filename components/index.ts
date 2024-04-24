5d585fb571222d4497e7cf816c5d5729f484e77b
wallacewong <wallacepuit@gmail.com>
2024-04-24 16:17:30 +0800

    feat: add product item detail in checkout screen

--- components/index.ts
+++ components/index.ts
@@ -3,11 +3,13 @@ import BannerCarousel from './BannerCarousel'
 import AnimatedTabs from './AnimatedTabs'
 import ProductCard from './ProductCard'
 import Spinner from './Spinner'
+import CartItemCard from './CartItemCard'
 
 export {
   ProductCard,
   ImageGallery,
   BannerCarousel,
   AnimatedTabs,
-  Spinner
+  Spinner,
+  CartItemCard
 }
