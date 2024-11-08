86b26c35383734b495aecda3b4aabca820dc0ea7
wallacewong <wallacepuit@gmail.com>
2024-11-08 17:29:44 +0000

    feat: add coupon sorting

--- app/(app)/(root)/(tabs)/coupons.tsx
+++ app/(app)/(root)/(tabs)/coupons.tsx
@@ -1,15 +1,23 @@
-import { AntDesign } from '@expo/vector-icons';
+import { AntDesign, MaterialCommunityIcons } from '@expo/vector-icons';
 import { tokens } from '@tamagui/themes';
 import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 import { Link } from 'expo-router';
 import moment from 'moment';
 import { useState } from 'react';
-import { SectionList, RefreshControl, FlatList, TouchableOpacity } from 'react-native';
-import { Image, SizableText, XStack, YStack } from 'tamagui';
+import {
+  SectionList,
+  RefreshControl,
+  FlatList,
+  TouchableOpacity,
+  SafeAreaView,
+} from 'react-native';
+import { Image, ScrollView, SizableText, XStack, YStack } from 'tamagui';
 import { Spinner } from '~/components';
 import { getCredit, getShop, listCoupons } from '~/api';
 import { useAuth, useLocale } from '~/hooks';
-import { Badge, Container, StyledButton } from '~/tamagui.config';
+import { Badge, Container, StyledButton, Title } from '~/tamagui.config';
+import { Coupon } from '~/types';
+import ActionSheet from '~/components/ActionSheet';
 
 const Coupons = () => {
   const { t } = useLocale();
@@ -22,6 +30,9 @@ const Coupons = () => {
     { label: t('creditLowToHigh'), value: 'credit' },
   ];
   const [selectedSortOption, setSelectedSortOption] = useState(sortOptions[0].value);
+  const [isSortingSheetOpen, setIsSortingSheetOpen] = useState(false);
+  const [sortingSheetPosition, setSortingSheetPoistion] = useState(0);
+
   const { data: shop } = useQuery({ queryKey: ['shop'], queryFn: getShop });
   const { data: credit, refetch: refetchCredit } = useQuery({
     queryKey: ['credit', token],
@@ -59,14 +70,25 @@ const Coupons = () => {
   };
 
   const onEndReached = () => {
-    console.log('onEndReached');
+    fetchMoreCoupons();
   };
 
   const renderCoupons = ({ item }: { item: Coupon }) => {
     return (
       <Link href={`/coupon/${item._id}`} asChild>
-        <TouchableOpacity style={{ flex: 0.5 }}>
-          <YStack flex={1} p="$4">
+        <TouchableOpacity style={{ width: '48%' }}>
+          <YStack
+            backgroundColor={'white'}
+            flex={1}
+            p="$4"
+            borderRadius={'$radius.3'}
+            shadowColor={'black'}
+            shadowOffset={{
+              height: 2,
+              width: 0,
+            }}
+            shadowOpacity={0.25}
+            shadowRadius={3.84}>
             <Image
               resizeMode="contain"
               aspectRatio={1}
@@ -90,80 +112,91 @@ const Coupons = () => {
       </Link>
     );
   };
-  const renderSectionItem = ({ item, index, section }) => {
-    switch (section.key) {
-      case 'cover':
-        const cover = shop.couponCover;
-        return (
-          <YStack>
-            <Image aspectRatio={16 / 9} source={{ uri: cover }} width={'100%'} />
-            <Badge position="absolute" bottom={22} left={22}>
-              <SizableText size={8} color="#fff">
-                {t('credit')}: {credit}
-              </SizableText>
-            </Badge>
-          </YStack>
-        );
-      case 'menu':
-        return (
-          <YStack p={'$4'} flex={1} alignItems="flex-end" justifyContent="flex-end">
-            <StyledButton>
-              {
-                sortOptions.find((s) => {
-                  return s.value == selectedSortOption;
-                })?.label
-              }
-            </StyledButton>
-          </YStack>
-        );
-      case 'coupons':
-        return (
-          <FlatList
-            data={couponsData}
-            renderItem={renderCoupons}
-            numColumns={2}
-            keyExtractor={(item, index) => index.toString()}
-            style={{ flex: 1 }}
-            columnWrapperStyle={{
-              flex: 1,
-              justifyContent: 'space-between',
-            }}
-            ListFooterComponent={() => {
-              if (!isCouponsFetching && !isFetchingMoreCoupons) {
-                return null;
-              }
-              return <Spinner color="$color.primary" />;
-            }}
-            ListEmptyComponent={() => {
-              if (isCouponsFetching || isFetchingMoreCoupons) {
-                return null;
-              }
-              return <Container alignItems="center"></Container>;
-            }}
-          />
-        );
-      default:
-        return <></>;
-    }
-  };
 
   return (
-    <YStack flex={1}>
-      <SectionList
-        refreshControl={<RefreshControl refreshing={false} onRefresh={() => onRefresh()} />}
-        style={{
-          backgroundColor: '#fff',
-        }}
-        renderItem={(item) => renderSectionItem(item)}
-        onEndReached={() => onEndReached()}
-        sections={[
-          { key: 'cover', data: [''] },
-          { key: 'menu', data: [''] },
-          { key: 'coupons', data: [''] },
-        ]}
-        keyExtractor={(item, index) => item + index}
-      />
-    </YStack>
+    <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
+      <ScrollView
+        bg="white"
+        refreshControl={<RefreshControl refreshing={false} onRefresh={onRefresh} />}>
+        <YStack>
+          <Image aspectRatio={16 / 9} source={{ uri: shop.couponCover }} width={'100%'} />
+          <Badge position="absolute" bottom={22} left={22}>
+            <SizableText size={8} color="#fff">
+              {t('credit')}: {credit}
+            </SizableText>
+          </Badge>
+        </YStack>
+        <YStack p={'$4'} flex={1} alignItems="flex-end" justifyContent="flex-end">
+          <StyledButton onPress={() => setIsSortingSheetOpen(true)}>
+            {
+              sortOptions.find((s) => {
+                return s.value == selectedSortOption;
+              })?.label
+            }
+          </StyledButton>
+        </YStack>
+        <FlatList
+          onEndReached={onEndReached}
+          data={couponsData}
+          renderItem={renderCoupons}
+          numColumns={2}
+          keyExtractor={(item, index) => index.toString()}
+          style={{ flex: 1 }}
+          columnWrapperStyle={{
+            flex: 1,
+            padding: 12,
+            justifyContent: 'space-between',
+          }}
+          ListFooterComponent={() => {
+            if (!isCouponsFetching && !isFetchingMoreCoupons) {
+              return null;
+            }
+            return (
+              <XStack flex={1} space="$2" alignItems="center" justifyContent="center">
+                <Spinner color="$color.primary" />
+                <SizableText color="slategrey">{t('loading')}</SizableText>
+              </XStack>
+            );
+          }}
+          ListEmptyComponent={() => {
+            if (isCouponsFetching || isFetchingMoreCoupons) {
+              return null;
+            }
+            return (
+              <Container alignItems="center">
+                <AntDesign name="folderopen" size={120} color="$666" />
+                <Title>{t('emptyContent')}</Title>
+              </Container>
+            );
+          }}
+        />
+      </ScrollView>
+      <ActionSheet
+        isSheetOpen={isSortingSheetOpen}
+        setIsSheetOpen={setIsSortingSheetOpen}
+        sheetPosition={sortingSheetPosition}
+        setSheetPosition={setSortingSheetPoistion}>
+        <ScrollView>
+          <YStack space={'$4'}>
+            {sortOptions.map((option, index) => {
+              const isSelected = option.value == selectedSortOption;
+              return (
+                <StyledButton
+                  onPress={() => {
+                    setIsSortingSheetOpen(false);
+                    setSelectedSortOption(option.value);
+                  }}
+                  key={option.value + index.toString()}
+                  width={'100%'}
+                  backgroundColor={isSelected ? '$color.primary' : 'lightslategrey'}>
+                  {option.label}
+                </StyledButton>
+              );
+            })}
+          </YStack>
+        </ScrollView>
+      </ActionSheet>
+    </SafeAreaView>
   );
 };
 
