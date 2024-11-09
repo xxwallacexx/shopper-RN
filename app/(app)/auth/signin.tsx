9617310270a7273f5cbf03548157d04e4356a040
wallacewong <wallacepuit@gmail.com>
2024-11-09 11:39:27 +0000

    feat: add login page

--- app/(app)/auth/signin.tsx
+++ app/(app)/auth/signin.tsx
@@ -1,89 +1,80 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query"
-import { useRouter } from "expo-router"
-import { useState } from "react"
-import { SafeAreaView } from "react-native"
-import Toast from "react-native-toast-message"
-import { Input } from "tamagui"
-import { Form, Label, YStack } from "tamagui"
-import { ScrollView } from "tamagui"
-import { login } from "~/api"
-import { Spinner } from "~/components"
-import { useAuth, useLocale } from "~/hooks"
-import { StyledButton } from "~/tamagui.config"
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useRouter } from 'expo-router';
+import { useState } from 'react';
+import { SafeAreaView } from 'react-native';
+import Toast from 'react-native-toast-message';
+import { Input } from 'tamagui';
+import { Form, Label, YStack } from 'tamagui';
+import { ScrollView } from 'tamagui';
+import { login } from '~/api';
+import { Spinner } from '~/components';
+import { useAuth, useLocale } from '~/hooks';
+import { StyledButton } from '~/tamagui.config';
 
 const Signin = () => {
-  const { t } = useLocale()
-  const { signin } = useAuth()
-  const router = useRouter()
-  const queryClient = useQueryClient()
-
+  const { t } = useLocale();
+  const { signin } = useAuth();
+  const router = useRouter();
+  const queryClient = useQueryClient();
 
   const { isPending: isSubmitting, mutate: loginMutate } = useMutation({
-    mutationFn: ({ email, password }: { email: string, password: string }) => {
-      return login(email, password)
+    mutationFn: ({ email, password }: { email: string; password: string }) => {
+      return login(email, password);
     },
     onSuccess: async (res) => {
-      await signin(res.token, res.tokenExpAt)
-      queryClient.invalidateQueries({ queryKey: ['profile'] })
-      router.replace("/(app)/(root)/(tabs)/profile")
+      await signin(res.token, res.tokenExpAt);
+      queryClient.invalidateQueries({ queryKey: ['profile'] });
+      router.replace('/(app)/(root)/(tabs)/profile');
     },
     onError: (e) => {
-      console.log(e)
-      const error = e as Error
+      console.log(e);
+      const error = e as Error;
       Toast.show({
         type: 'error',
         text1: t(error.message),
       });
-    }
-  })
+    },
+  });
 
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
 
-  const [email, setEmail] = useState<string>()
-  const [password, setPassword] = useState<string>()
-
-  const disabled = !email || email == "" || !password || password == ""
+  const disabled = !email || email == '' || !password || password == '';
 
   const onSubmit = () => {
-    if (!email || !password) return
-    loginMutate({ email, password })
-  }
+    if (!email || !password) return;
+    loginMutate({ email, password });
+  };
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <ScrollView>
-        <Form
-          onSubmit={onSubmit}
-        >
+      <ScrollView bg="white" p="$4">
+        <Form onSubmit={onSubmit}>
           <YStack space="$2" p="$2">
-            <Label>
-              {t('email')}
-            </Label>
+            <Label>{t('email')}</Label>
             <Input
-              autoCapitalize='none'
+              autoCapitalize="none"
               disabled={isSubmitting}
-              borderColor={"lightgrey"}
-              backgroundColor={"whitesmoke"}
+              borderColor={'lightgrey'}
+              backgroundColor={'whitesmoke'}
               onChangeText={(value) => setEmail(value)}
             />
-            <Label>
-              {t('password')}
-            </Label>
+            <Label>{t('password')}</Label>
             <Input
-              autoCapitalize='none'
+              autoCapitalize="none"
               disabled={isSubmitting}
-              borderColor={"lightgrey"}
-              backgroundColor={"whitesmoke"}
+              borderColor={'lightgrey'}
+              backgroundColor={'whitesmoke'}
               onChangeText={(value) => setPassword(value)}
             />
             <Form.Trigger asChild disabled={isSubmitting}>
               <StyledButton
                 disabled={disabled}
-                alignSelf='center'
+                alignSelf="center"
                 m="$4"
                 w="$20"
                 style={{ opacity: isSubmitting ? 0.5 : 1 }}
-                icon={isSubmitting ? <Spinner /> : null}
-              >
-                Submit
+                icon={isSubmitting ? <Spinner /> : null}>
+                {t('confirm')}
               </StyledButton>
             </Form.Trigger>
             <Spinner />
@@ -91,8 +82,7 @@ const Signin = () => {
         </Form>
       </ScrollView>
     </SafeAreaView>
+  );
+};
 
-  )
-}
-
-export default Signin
+export default Signin;
