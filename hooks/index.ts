648124a3a777c589e58348f7d9f1feac46b8e064
wallacewong <wallacepuit@gmail.com>
2025-06-08 17:03:28 +0100

    refactor: split components

--- hooks/index.ts
+++ hooks/index.ts
@@ -4,5 +4,6 @@ import { useDebounce } from './useDebounce';
 import { useLocale } from './useLocale';
 import { useMutationWithErrorHandling } from './useMutationWithErrorHandling';
 export * from './useInfiniteData';
+export * from './useProductData';
 
 export { useLocale, useAuth, useCountdown, useDebounce, useMutationWithErrorHandling };
